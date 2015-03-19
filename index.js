332b0831fbeaeb0b81c1bd7d6e2e2e168b41a690
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-03-19 10:16:27 -0300

    Add body parsing

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 (function () {
   'use strict';
 
+
   var app = require('express')();
   var cors = require('cors');
+  var bodyParser = require('body-parser');
   var http = require('http')
     .Server(app);
   var API_TOKEN = 'HNRO7R4NKRZrgbVf4JvhoeLY';
 
   app.use(cors());
+
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
 
   app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:1337');
@@ -18,6 +24,21 @@
   app.post('/', function (req, res) {
     console.log(req.body);
     console.log('Received a hodor message!');
+
+    var userName = req.body.user_name;
+    var botPayload = {
+      text: 'Hello, ' + userName + '!'
+    };
+
+    // avoid infinite loop
+    if (userName !== 'slackbot') {
+      return res.status(200)
+        .json(botPayload);
+    } else {
+      return res.status(200)
+        .end();
+    }
+
     res.send('Thanks!');
   });
 
